fc25a40e68626a7f4afffb86725435bbd887f93a
Asbjørn Steinskog <asbjorn@steinskog.me>
2014-10-20 22:48:31 -0700

    Add enqueue and worker for task

---
+++ src/main/java/project/project/Worker.java
@@ -0,0 +1,33 @@
+package project.project;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+// The Worker servlet should be mapped to the "/worker" URL.
+public class Worker extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String key = request.getParameter("key");
+        // Do something with key.
+        
+        //String guestbookName = req.getParameter("guestbookName");
+        Key taskKey = KeyFactory.createKey("Task", key);
+        Date date = new Date();
+        Entity task = new Entity("Task", taskKey);
+        task.setProperty("date", date);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(task);
+    }
+}
