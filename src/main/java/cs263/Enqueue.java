b30103923453575276e979769b6beeee1c01c686
Asbjørn Steinskog <asbjorn@steinskog.me>
2014-11-16 16:56:19 -0800

    Convert JSON object to String hashmap

--- src/main/java/cs263/Enqueue.java
+++ src/main/java/cs263/Enqueue.java
@@ -6,6 +6,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
 import static com.google.appengine.api.taskqueue.TaskOptions.Builder.*;
@@ -32,13 +36,12 @@ public class Enqueue extends HttpServlet {
 		}
         
         //CONVERT TO GSON
-    	Gson gson = new Gson();
+    	Gson gson = new Gson(); 
+    	
+    	HashMap<String,String> map = new Gson().fromJson(json_data, new TypeToken<HashMap<String, String>>(){}.getType());
 
- 
-		//convert the json string back to object
-		Gson obj = gson.fromJson(json_data, Gson.class);
-		System.out.println("OBJECT");
-  		System.out.println(obj);
+        System.out.println(map.get("Response"));
+		
         System.out.println("STRING");
         System.out.println(json_data);
 
